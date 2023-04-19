cd56999305091a8d50bfa03e704ad11dbab4d3c3
stats-dev <hohun94@naver.com>
2023-04-19 09:51:43 +0900

    assignment solution sql updates

--- assignments/2023.04.18/25_DML_DDL_Question.sql
+++ assignments/2023.04.18/25_DML_DDL_Question.sql
@@ -7,6 +7,19 @@ UPDATE ST_TEMP
 
 SELECT * 
     FROM ST_TEMP;
+    
+
+
+CREATE TABLE ST_TEMP2
+    AS SELECT * FROM STUDENT;
+
+UPDATE ST_TEMP2
+    SET
+        AVR = AVR * 4.5 / 4.0;
+
+SELECT *
+    FROM ST_TEMP2;
+
 
 --2) PROFESSOR 테이블을 참조하여 PF_TEMP 테이블을 만들고 모든 교수의 부임일자를 100일 앞으로 수정하세요
 CREATE TABLE PF_TEMP
@@ -18,15 +31,41 @@ UPDATE PF_TEMP
 SELECT *
     FROM PF_TEMP;
     
+    
+    
+    
+CREATE TABLE PF_TEMP2
+    AS SELECT * FROM PROFESSOR;
+
+UPDATE PF_TEMP2
+    SET
+        HIREDATE = HIREDATE - 100;
+    
+SELECT *
+    FROM PF_TEMP2;
+    
+COMMIT;    
+    
 
 --3) ST_TEMP 테이블에서 화학과 2학년 학생의 정보를 삭제하세요
 DELETE FROM ST_TEMP
     WHERE MAJOR = '화학'
         AND SYEAR = 2;
 
+
+DELETE FROM ST_TEMP2
+    WHERE MAJOR = '화학'
+        AND SYEAR = 2;
+
+
 --4) PF_TEMP 테이블에서 조교수의 정보를 삭제하세요
 DELETE FROM PF_TEMP
     WHERE ORDERS = '조교수';
+
+
+DELETE FROM PF_TEMP2
+    WHERE ORDERS = '조교수';
+
 
 --5) EMP 테이블을 참조하여 EMP2 테이블을 만들고 DNO = 30인 사원의 보너스를 15프로 상승시킨 값으로 변경하시고 
 --   DNO = 20인 사원의 급여를 10프로 상승시킨 값으로 변경하세요.
@@ -37,7 +76,25 @@ UPDATE EMP2
     SET COMM = COMM * 1.15
     WHERE DNO = '30';
 
-   
+UPDATE EMP2
+    SET SAL = SAL * 1.1
+    WHERE DNO = '20';
+    
+CREATE TABLE EMP3
+    AS SELECT * FROM EMP;
+    
+UPDATE EMP3
+    SET
+        COMM = COMM * 1.15
+    WHERE DNO = '20';
+    
+UPDATE EMP3
+    SET
+        SAL = SAL * 1.1
+    WHERE DNO = '30';
+    
+SELECT *
+    FROM EMP3;
 
 --6) 화학과 2학년 학생중 기말고사 성적의 등급이 A, B인 정보를 갖는 테이블 SCORE_STGR을 생성하세요.(SNO, SNAME, MAJOR, SYEAR, RESULT, GRADE)
 CREATE TABLE SCORE_STGR(SNO, SNAME, MAJOR, SYEAR, RESULT, GRADE)
@@ -57,6 +114,24 @@ CREATE TABLE SCORE_STGR(SNO, SNAME, MAJOR, SYEAR, RESULT, GRADE)
     
 --SELECT * FROM SCORE_STGR;
 
+CREATE TABLE SCORE_STGR2
+    AS SELECT SNO
+            , ST.SNAME
+            , ST.MAJOR
+            , ST.SYEAR
+            , SC.RESULT
+            , SG.GRADE
+        FROM STUDENT ST
+        NATURAL JOIN SCORE SC
+        JOIN SCGRADE SG
+        ON SC.RESULT BETWEEN SG.LOSCORE AND SG.HISCORE
+        WHERE ST.MAJOR = '화학'
+            AND ST.SYEAR = 2
+            AND SG.GRADE IN ('A', 'B');
+
+SELECT *
+    FROM SCORE_STGR2;
+
 --7) 생물학과 학생중 평점이 2.7이상인 학생이 수강중인 과목의 정보를 갖는 테이블 ST_COURSEPF를 생성하세요. (SNO, SNAME, CNO, CNAME, PNO, PNAME, AVR)
 CREATE TABLE ST_COURSEPF(SNO, SNAME, CNO, CNAME, PNO, PNAME, AVR)
     AS SELECT SNO
@@ -73,3 +148,22 @@ CREATE TABLE ST_COURSEPF(SNO, SNAME, CNO, CNAME, PNO, PNAME, AVR)
             AND ST.AVR >= 2.7;
 
 --SELECT * FROM ST_COURSEPF;
+--RESULT 추가해준 버전은 아래와 같다.
+--7-1) 생물학과 학생중 평점이 2.7이상인 학생이 수강중인 과목의 정보를 갖는 테이블 ST_COURSEPF를 생성하세요. (SNO, SNAME, CNO, CNAME, PNO, PNAME, RESULT, AVR)
+CREATE TABLE ST_COURSEPF2
+    AS SELECT SNO
+            , ST.SNAME
+            , CNO
+            , C.CNAME
+            , PNO
+            , P.PNAME
+            , SC.RESULT
+            , ST.AVR
+        FROM STUDENT ST
+        NATURAL JOIN SCORE SC
+        NATURAL JOIN COURSE C
+        NATURAL JOIN PROFESSOR P
+        WHERE ST.MAJOR = '생물'
+          AND ST.AVR >= 2.7;
+          
+SELECT * FROM ST_COURSEPF2;
