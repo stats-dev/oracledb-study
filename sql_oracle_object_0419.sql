73108a20e1b6050458885c68ad44ca49eac066a3
stats-dev <hohun94@naver.com>
2023-04-19 11:41:32 +0900

    view create or replace in sql

--- sql_oracle_object_0419.sql
+++ sql_oracle_object_0419.sql
@@ -35,7 +35,7 @@ SELECT *
     FROM ALL_TABLES;
 
 --OWNER 컬럼이 없음.
---현재 접속한 사용자가     
+--현재 접속한 사용자가 소유한 테이블의 정보만 담고 있어서
 SELECT *
     FROM USER_TABLES;
     
@@ -90,4 +90,94 @@ SELECT L.OBJECT_ID --V$LOCKED_OBJECT
 
 --현 세션은 종료할 수 없다.    
 --ALTER SYSTEM KILL SESSION '504, 42188' IMMEDIATE;
+
+
+--1-2. 인덱스
+--인덱스 생성
+--STUDENT 테이블에 SNAME을 인덱스로 생성(비 고유 인덱스)
+CREATE INDEX STUDENT_SNAME_IDX
+        ON STUDENT(SNAME);
+
+--여러개의 컬럼을 선택하여 복합 인덱스 생성
+--PROFESSOR 테이블에 PNO, PNAME을 복합 인덱스로 생성
+CREATE INDEX PROFESSOR_PNO_PNAME_IDX
+ON PROFESSOR(PNO, PNAME); --두개 컬럼 선택해서 인덱스로 만든다.
+
+
+--수식으로 인덱스 생성
+--테이블에 여러개의 인덱스 생성 가능
+--STUDENT 테이블에 평점을 4.5로 환산한 평점 인덱스로 지정.
+--수식으로 만든 인덱스는 컬럼명이 표시되지 않고 시스템에서 자동으로 생성한 수식의 이름을 사용한다.
+CREATE INDEX STUDENT_CONVERT_AVR_IDX
+ON STUDENT(AVR * 4.5 / 4.0);
+
+--생성된 인덱스 확인
+--CONVERT IDX는 COLUMN_NAME이 아니라 시스템이 지정해준 이름으로 들어간 걸 확인할 수 있다.
+SELECT UIC.INDEX_NAME
+     , UIC.COLUMN_NAME
+     , UIC.COLUMN_POSITION
+     , UI.UNIQUENESS
+    FROM USER_INDEXES UI
+    JOIN USER_IND_COLUMNS UIC
+    ON UI.INDEX_NAME = UIC.INDEX_NAME
+    AND UI.TABLE_NAME IN ('STUDENT', 'PROFESSOR')
+    ORDER BY UI.TABLE_NAME, UIC.INDEX_NAME, UIC.COLUMN_POSITION;
+
+--인덱스 삭제: DROP INDEX 인덱스명;
+DROP INDEX STUDENT_SNAME_IDX;
+DROP INDEX PROFESSOR_PNO_PNAME_IDX;
+DROP INDEX STUDENT_CONVERT_AVR_IDX;
+
+
+SELECT *
+    FROM STUDENT
+    WHERE SNAME = '괴월';
+
+--일반적으로 대량의 데이터에서는 인덱스를 생성해서 조회하는 게 더 빠르게 실행된다고 한다!    
+CREATE INDEX STUDENT_SNAME_IDX
+        ON STUDENT(SNAME);
+
+SELECT *
+    FROM STUDENT
+    WHERE SNAME = '괴월';
+
+
+
+--1-3. VIEW
+--뷰의 생성
+--과목별 학과별 기말고사의 평균을 저장하는 뷰 생성
+CREATE OR REPLACE VIEW V_AVG_SCORE(
+    CNO,
+    CNAME,
+    MAJOR,
+    AVGRESULT --컬럼명을 커스터마이징해서 사용할 수 있기 때문에 원천 테이블의 컬럼명을 노출하지 않을 수 있다.
+) AS (
+        --위에서 지정한 컬럼의 순서와 개수가 일치해야 한다.
+        SELECT CNO
+             , C.CNAME
+             , ST.MAJOR
+             , ROUND(AVG(SC.RESULT), 2)
+            FROM COURSE C
+            NATURAL JOIN SCORE SC
+            JOIN STUDENT ST
+            ON SC.SNO = ST.SNO
+            GROUP BY CNO, C.CNAME, ST.MAJOR
+);
+
+--뷰를 조회
+--과목별 평균 점수해야 뷰를 조회 가능함.
+
+SELECT *
+    FROM V_AVG_SCORE;
+    
+
+
+
+
+
+
+
+
+
+
 
