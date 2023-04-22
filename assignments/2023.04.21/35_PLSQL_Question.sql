bd195dd43575298d9e6aff818808b498f2016bee
stats-dev <hohun94@naver.com>
2023-04-22 10:16:53 +0900

    assignments solution for pl/sql related to basic loop and bulk collect into options

--- assignments/2023.04.21/35_PLSQL_Question.sql
+++ assignments/2023.04.21/35_PLSQL_Question.sql
@@ -1,22 +1,29 @@
 --1) 과목번호, 과목이름, 교수번호, 교수이름을 담을 수 있는 변수들을 선언하고 
 --   유기화학 과목의 과목번호, 과목이름, 교수번호, 교수이름을 출력하세요.
-CREATE TABLE PRO_ALL(
-        CNO,
-        CNAME,
-        PNO,
-        PNAME
-        ) AS (
-        SELECT C.CNO
-             , C.CNAME
-             , PNO
-             , P.PNAME
-            FROM COURSE C
-            NATURAL JOIN PROFESSOR P
-);
+DECLARE
+        CNO COURSE.CNO%TYPE;
+        CNAME COURSE.CNAME%TYPE;
+        PNO PROFESSOR.PNO%TYPE;
+        PNAME PROFESSOR.PNAME%TYPE;
+BEGIN
+    SELECT C.CNO
+         , C.CNAME
+         , PNO
+         , P.PNAME
+        INTO CNO, CNAME, PNO, PNAME --괄호치지 않고 그대로 콤마로 구분해서 넣으면 된다.
+        FROM COURSE C
+        NATURAL JOIN PROFESSOR P
+        WHERE C.CNAME = '유기화학';
+            
+    DBMS_OUTPUT.PUT_LINE(CNO);
+    DBMS_OUTPUT.PUT_LINE(CNAME);
+    DBMS_OUTPUT.PUT_LINE(PNO);
+    DBMS_OUTPUT.PUT_LINE(PNAME);
+END;
+/
 
-SELECT *
-    FROM PRO_ALL
-    WHERE CNAME = '유기화학';
+
+
 
 
 --2) 위 데이터들을 레코드로 선언하고 출력하세요.
@@ -32,15 +39,16 @@ DECLARE
     PROREC PRO_REC;
 BEGIN
     
-    SELECT CNO
-         , CNAME
+    SELECT C.CNO
+         , C.CNAME
          , PNO
-         , PNAME
+         , P.PNAME
         INTO PROREC.CNO
            , PROREC.CNAME
            , PROREC.PNO
            , PROREC.PNAME
-        FROM PRO_ALL
+        FROM COURSE C
+        NATURAL JOIN PROFESSOR P
         WHERE CNAME = '유기화학';
     
 --    PROREC.CNO := 1212;
@@ -58,7 +66,42 @@ END;
 
 --3) 과목번호, 과목이름, 과목별 평균 기말고사 성적을 갖는 레코드의 배열을 만들고
 --   기본 LOOP문을 이용해서 모든 과목의 과목번호, 과목이름, 과목별 평균 기말고사 성적을 출력하세요.
+DECLARE
+    TYPE SCORE_REC IS RECORD(
+        CNO COURSE.CNO%TYPE,
+        CNAME COURSE.CNAME%TYPE,
+        AVGRES NUMBER NOT NULL := 0
+    );
+    
+    TYPE SCORE_ARRAY IS TABLE OF SCORE_REC
+    INDEX BY PLS_INTEGER;
+    
+    SCOARR SCORE_ARRAY;
+    
+    IDX NUMBER := 1;
+    
+BEGIN
+    LOOP
+        SELECT CNO
+             , C.CNAME
+             , AVG(SC.RESULT) AS AVGRES
+            BULK COLLECT INTO SCOARR
+            FROM COURSE C
+            NATURAL JOIN SCORE SC
+            GROUP BY CNO, C.CNAME;
+     
+        DBMS_OUTPUT.PUT_LINE(SCOARR(IDX).CNO);
+        DBMS_OUTPUT.PUT_LINE(SCOARR(IDX).CNAME);
+        DBMS_OUTPUT.PUT_LINE(SCOARR(IDX).AVGRES);
+        
+        IDX := IDX + 1;
+        EXIT WHEN IDX > SCOARR.COUNT;
+    END LOOP;
+END;
+/
 
+
+-- 기본 LOOP로는 해결이 되지 않아서, FOR LOOP를 사용했습니다. 한 행씩 반복해서 가져올 수 없었습니다.
 DECLARE
     TYPE SCORE_REC IS RECORD(
         CNO COURSE.CNO%TYPE,
@@ -76,7 +119,7 @@ DECLARE
 
 
 BEGIN
-    FOR SCO IN(
+    FOR STROW IN(
         SELECT CNO
              , C.CNAME
              , AVG(SC.RESULT) AS AVGRES
@@ -85,7 +128,7 @@ BEGIN
             GROUP BY CNO, C.CNAME
             ) LOOP
       
-           SCOARR(IDX) := SCORE_REC(SCO.CNO, SCO.CNAME, SCO.AVGRES);
+           SCOARR(IDX) := SCORE_REC(STROW.CNO, STROW.CNAME, STROW.AVGRES);
 --            WHERE ROWNUM = IDX;
                     
         DBMS_OUTPUT.PUT_LINE(SCOARR(IDX).CNO);
@@ -93,10 +136,17 @@ BEGIN
         DBMS_OUTPUT.PUT_LINE(SCOARR(IDX).AVGRES);
         
         IDX := IDX + 1;
-        EXIT WHEN SCO.CNO IS NULL;
+        EXIT WHEN STROW.CNO IS NULL;
     END LOOP;
 END;
 /
+
+
+
+
+
+
+
 
 
 --4) 학생번호, 학생이름과 학생별 평균 기말고사 성적을 갖는 테이블 T_STAVGSC를 만들고
