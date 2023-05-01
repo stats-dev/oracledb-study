d70875e8bfb9e64ee8a4907d3f3c5c57cd4565dd
stats-dev <hohun94@naver.com>
2023-05-01 10:02:59 +0900

    answer the assignements with cursor and table

--- assignments/2023.04.21/35_PLSQL_Question.sql
+++ assignments/2023.04.21/35_PLSQL_Question.sql
@@ -22,12 +22,41 @@ BEGIN
 END;
 /
 
+SET SERVEROUTPUT ON;
+
+
+DECLARE
+    --변수 4개 선언
+    CNO VARCHAR2(8);
+    CNAME VARCHAR2(20);
+    PNO VARCHAR2(8);
+    PNAME VARCHAR2(20);
+BEGIN
+    SELECT C.CNO
+         , C.CNAME
+         , PNO
+         , P.PNAME
+        INTO CNO
+            , CNAME
+            , PNO
+            , PNAME --한행씩 가져와서 넣겠다!
+        FROM COURSE C
+        NATURAL JOIN PROFESSOR P
+        WHERE C.CNAME = '유기화학';
+    
+        DBMS_OUTPUT.PUT_lINE(CNO);
+        DBMS_OUTPUT.PUT_lINE(CNAME);
+        DBMS_OUTPUT.PUT_lINE(PNO);
+        DBMS_OUTPUT.PUT_lINE(PNAME);
+END;
+/
+
 
 
 
 
 --2) 위 데이터들을 레코드로 선언하고 출력하세요.
-SET SERVEROUTPUT ON;
+--SET SERVEROUTPUT ON;
 DECLARE
     TYPE PRO_REC IS RECORD(
         CNO NUMBER,
@@ -63,6 +92,41 @@ BEGIN
 
 END;
 /
+
+
+DECLARE
+    TYPE COURPF_REC IS RECORD(
+        CNO COURSE.CNO%TYPE,
+        CNAME COURSE.CNAME%TYPE,
+        PNO COURSE.PNO%TYPE,
+        PNAME PROFESSOR.PNAME%TYPE
+    );
+    
+   COURPfREC COURPF_REC;
+   
+BEGIN
+    
+    SELECT C.CNO
+         , C.CNAME
+         , PNO
+         , P.PNAME
+        INTO COURPfREC.CNO
+           , COURPfREC.CNAME
+           , COURPfREC.PNO
+           , COURPfREC.PNAME
+        FROM COURSE C
+        NATURAL JOIN PROFESSOR P
+        WHERE CNAME = '유기화학';
+    
+    DBMS_OUTPUT.PUT_LINE(COURPfREC.CNO);
+    DBMS_OUTPUT.PUT_LINE(COURPfREC.CNAME);
+    DBMS_OUTPUT.PUT_LINE(COURPfREC.PNO);
+    DBMS_OUTPUT.PUT_LINE(COURPfREC.PNAME);
+
+END;
+/
+    
+
 
 --3) 과목번호, 과목이름, 과목별 평균 기말고사 성적을 갖는 레코드의 배열을 만들고
 --   기본 LOOP문을 이용해서 모든 과목의 과목번호, 과목이름, 과목별 평균 기말고사 성적을 출력하세요.
@@ -142,9 +206,45 @@ END;
 /
 
 
+--해설
+--3) 과목번호, 과목이름, 과목별 평균 기말고사 성적을 갖는 레코드의 배열을 만들고
+--   기본 LOOP문을 이용해서 모든 과목의 과목번호, 과목이름, 과목별 평균 기말고사 성적을 출력하세요.
+DECLARE
+    TYPE COURSEAVG_REC IS RECORD(
+        CNO COURSE.CNO%TYPE,
+        CNAME COURSE.CNAME%TYPE,
+        AVGRES NUMBER(5, 2)
+    );
 
+    TYPE COURSEAVG_ARR IS TABLE OF COURSEAVG_REC
+    INDEX BY PLS_INTEGER;
+    
+    --배열 타입의 변수명 설정
+    COURSEAVGARR COURSEAVG_ARR;
+    
+    IDX NUMBER := 1; --시작번호 1번
 
-
+BEGIN
+    FOR i IN ( --i.CNAME 이런식으로 꺼내서 평균값이 꺼내지도록 설정한다.
+                SELECT CNO
+                     , C.CNAME
+                     , ROUND(AVG(SC.RESULT), 2) AS AVGRES
+                    FROM COURSE C
+                    NATURAL JOIN SCORE SC
+                    GROUP BY CNO, C.CNAME
+                ) LOOP
+            COURSEAVGARR(IDX).CNO := i.CNO;
+            COURSEAVGARR(IDX).CNAME := i.CNAME;
+            COURSEAVGARR(IDX).AVGRES := i.AVGRES;
+            
+            DBMS_OUTPUT.PUT_LINE(COURSEAVGARR(IDX).CNO);
+            DBMS_OUTPUT.PUT_LINE(COURSEAVGARR(IDX).CNAME);
+            DBMS_OUTPUT.PUT_LINE(COURSEAVGARR(IDX).AVGRES);
+            
+            IDX := IDX + 1;
+    END LOOP;
+END;
+/
 
 
 
@@ -186,3 +286,40 @@ END;
 
 SELECT *
     FROM T_STAVGSC;
+    
+--ANSWER
+CREATE  TABLE T_STAVGSC1 (
+    SNO NUMBER,
+    SNAME VARCHAR2(20),
+    AVGRES NUMBER(5, 2)
+);
+
+--선언부
+DECLARE
+    CURSOR CUR_STAVGSC IS
+    SELECT ST.SNO
+         , ST.SNAME
+         , NVL(ROUND(AVG(SC.RESULT), 2), 0) AS AVGRES --NULL이면 0점으로 처리
+        FROM STUDENT ST
+        LEFT JOIN SCORE SC
+        ON ST.SNO = SC.SNO
+        GROUP BY ST.SNO, ST.SNAME; --쿼리 결과를 커서에 담는다.
+        
+    ROW_STAVGSC T_STAVGSC1%ROWTYPE;
+BEGIN
+    OPEN CUR_STAVGSC; --커서명으로 오픈!
+    
+    LOOP
+        FETCH CUR_STAVGSC INTO ROW_STAVGSC; --로우타입 한줄 데이터에 넣는다.
+        
+        EXIT WHEN CUR_STAVGSC%NOTFOUND; --더이상 커서 발견 못하면 루프문 종료
+        
+        INSERT INTO T_STAVGSC1
+        VALUES ROW_STAVGSC; --로우타입에 넣는다.
+    
+    END LOOP;
+END;
+/
+
+SELECT *
+    FROM T_STAVGSC1;
